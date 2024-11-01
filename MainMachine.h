6161367cf4f0c5b3ef70396417429d43d5437734
عبدالرحمن خالد اله رش <162501346+3BZO-58@users.noreply.github.com>
2024-11-01 19:34:47 +0200

    Add files via upload

---
+++ MainMachine.h
@@ -0,0 +1,23 @@
+#ifndef MAINMACHINE_H
+#define MAINMACHINE_H
+#include "BUS.h"
+#include <string>
+#include "BUS.cpp"
+#include <fstream>
+#include "Memory.h"
+#include "Rigister.h"
+class MainMachine {
+    Memory m;
+    Rigister r;
+    fstream file;
+    string fileName;
+public:
+    void dataDisplay();
+    void dataReset();
+    void getFileNmae(string Name);
+    void dataRead();
+};
+
+
+
+#endif //MAINMACHINE_H
