7f33cb84e4ef7e3e180e11afeee59d797d6ba88a
mohammed junaid <mohammedshahd2006@gmail.com>
2024-11-01 19:55:12 +0300

    adding functions to the ALU

--- ALU.cpp
+++ ALU.cpp
@@ -1,12 +1,33 @@
 #include "ALU.h"
-
-void ALU:: add_complement(int ind1 , int ind2 , int ind3 , Rigister Rig){
+#include <math.h>
+void ALU:: add_complement(int ind1 , int ind2 , int ind3 , Rigister &Rig){
+    string stNum = HexTobin(Rig.get_value(ind1));
+    string ndNum = HexTobin(Rig.get_value(ind2));
+    string res ;
+    char carry = '0' ;
+    for(int i = 7 ; i >= 0 ; i--){
+        if((stNum[i] == '1' && ndNum[i] == '0' && carry == '0') || (stNum[i] == '0' && ndNum[i] == '1' && carry == '0') || (stNum[i] == '0' && ndNum[i] == '0' && carry == '1')){
+            carry = '0' ;
+            res += '1' ;
+        }
+        else if(stNum[i] == '0' && ndNum[i] == '0' && carry == '0'){
+            carry = '0' ;
+            res += '0' ;
+        }else{
+            if(carry == '0'){
+                carry = '1' ;
+                res += '0' ;
+            }else{
+                carry = '1' ;
+                res += '1' ; 
+            }
+        }
+    }
+}
+void ALU::add_flowting(int ind1 , int ind2 , int ind3 , Rigister &Rig){
 
 }
-void ALU::add_flowting(int ind1 , int ind2 , int ind3 , Rigister Rig){
-
-}
-void ALU::BitOr(int ind1 , int ind2 , int ind3 , Rigister Rig){
+void ALU::BitOr(int ind1 , int ind2 , int ind3 , Rigister &Rig){
     string stNum = HexTobin(Rig.get_value(ind1));
     string ndNum = HexTobin(Rig.get_value(ind2));
     string res ;
@@ -15,9 +36,9 @@ void ALU::BitOr(int ind1 , int ind2 , int ind3 , Rigister Rig){
         else res += '0' ;
     }
     pair<char,char> FinalRes = BintoHex(res) ;
-    // here i should pass the number of the rigister i want to save the result in
+    Rig.set_value(ind3 , FinalRes) ;
 }
-void ALU::BitAnd(int ind1 , int ind2 , int ind3 , Rigister Rig){
+void ALU::BitAnd(int ind1 , int ind2 , int ind3 , Rigister & Rig){
     string stNum = HexTobin(Rig.get_value(ind1));
     string ndNum = HexTobin(Rig.get_value(ind2));
     string res ;
@@ -26,9 +47,9 @@ void ALU::BitAnd(int ind1 , int ind2 , int ind3 , Rigister Rig){
         else res += '0' ;
     }
     pair<char,char> FinalRes = BintoHex(res) ;
-    // here i should pass the number of the rigister i want to save the result in
+    Rig.set_value(ind3 , FinalRes) ;
 }
-void ALU::BitXor(int ind1 , int ind2 , int ind3 , Rigister Rig){
+void ALU::BitXor(int ind1 , int ind2 , int ind3 , Rigister & Rig){
     string stNum = HexTobin(Rig.get_value(ind1));
     string ndNum = HexTobin(Rig.get_value(ind2));
     string res ;
@@ -38,9 +59,9 @@ void ALU::BitXor(int ind1 , int ind2 , int ind3 , Rigister Rig){
         else res += '0' ;
     }
     pair<char,char> FinalRes = BintoHex(res) ;
-    // here i should pass the number of the rigister i want to save the result in
+    Rig.set_value(ind3 , FinalRes) ;
 }
-void ALU::Rotate(int ind1 ,int Steps , Rigister Rig){
+void ALU::Rotate(int ind1 ,int Steps , Rigister & Rig){
     pair<char,char> FinalRes ;
     if(Steps % 2 == 1 ){
         FinalRes.first = Rig.get_value(ind1).second ;
@@ -49,9 +70,9 @@ void ALU::Rotate(int ind1 ,int Steps , Rigister Rig){
         FinalRes.first = Rig.get_value(ind1).first ;
         FinalRes.second = Rig.get_value(ind1).second ;
     }
-    // here i should pass the number of the rigister i want to save the result in
+    Rig.set_value(ind1 , FinalRes) ;
 }
-bool ALU::Greater(int ind1 , Rigister Rig){
+bool ALU::Greater(int ind1 , Rigister & Rig){
     pair<char,char> stPair = Rig.get_value(ind1) ;
     pair<char,char> ndPair = Rig.get_value(0) ;
     if(stPair.first > ndPair.first){
@@ -64,7 +85,7 @@ bool ALU::Greater(int ind1 , Rigister Rig){
         }
     }
 }
-bool ALU::EqualTo(int ind1 , Rigister Rig){
+bool ALU::EqualTo(int ind1 , Rigister & Rig){
     pair<char,char> stPair = Rig.get_value(ind1) ;
     pair<char,char> ndPair = Rig.get_value(0) ;
     if(stPair.first == ndPair.first && stPair.second == ndPair.second){
@@ -116,11 +137,53 @@ string ALU::HexTobin(pair<char , char> hexa){
     return bin ;
 }
 pair<char,char> ALU:: BintoHex(string num){
-
+    pair<char,char> hexa ;
+    string stPair = num.substr(0,4) ;
+    string ndPair = num.substr(4,4) ;
+    if(stPair == "0000" ) hexa.first = '0' ;
+    else if (stPair == "0001" ) hexa.first = '1' ;
+    else if (stPair == "0010" ) hexa.first = '2' ;
+    else if (stPair == "0011" ) hexa.first = '3' ;
+    else if (stPair == "0100" ) hexa.first = '4' ;
+    else if (stPair == "0101" ) hexa.first = '5' ;
+    else if (stPair == "0110" ) hexa.first = '6' ;
+    else if (stPair == "0111" ) hexa.first = '7' ;
+    else if (stPair == "1000" ) hexa.first = '8' ;
+    else if (stPair == "1001" ) hexa.first = '9' ;
+    else if (stPair == "1010" ) hexa.first = 'A' ;
+    else if (stPair == "1011" ) hexa.first = 'B' ;
+    else if (stPair == "1100" ) hexa.first = 'C' ;
+    else if (stPair == "1101" ) hexa.first = 'D' ;
+    else if (stPair == "1110" ) hexa.first = 'E' ;
+    else hexa.first = 'F' ;
+    if(ndPair == "0000" ) hexa.second = '0' ;
+    else if (ndPair == "0001" ) hexa.second = '1' ;
+    else if (ndPair == "0010" ) hexa.second = '2' ;
+    else if (ndPair == "0011" ) hexa.second = '3' ;
+    else if (ndPair == "0100" ) hexa.second = '4' ;
+    else if (ndPair == "0101" ) hexa.second = '5' ;
+    else if (ndPair == "0110" ) hexa.second = '6' ;
+    else if (ndPair == "0111" ) hexa.second = '7' ;
+    else if (ndPair == "1000" ) hexa.second = '8' ;
+    else if (ndPair == "1001" ) hexa.second = '9' ;
+    else if (ndPair == "1010" ) hexa.second = 'A' ;
+    else if (ndPair == "1011" ) hexa.second = 'B' ;
+    else if (ndPair == "1100" ) hexa.second = 'C' ;
+    else if (ndPair == "1101" ) hexa.second = 'D' ;
+    else if (ndPair == "1110" ) hexa.second = 'E' ;
+    else hexa.second = 'F' ;
+    return hexa ;
 }
-float ALU::BintoNum(string num){
-
+int ALU::BintoNum(string num){
+    int number ;
+    for(int i = num.size()-1 ; i >= 0 ; i--){
+        number += ((int(num[i])-48) * pow(2,num.size()-1 - i ));
+    }
+    return number ;
 }
 string ALU::NumtoBin(float num){
+
+}
+float ALU :: FBintoNum(string num ){
 
 }
--- ALU.h
+++ ALU.h
@@ -1,24 +1,27 @@
 #ifndef VOL_ASSIGNMNT_ALU_H
 #define VOL_ASSIGNMNT_ALU_H
 
-#include "Rigister.h"
-#include "Rigister.cpp"
+#include "BUS.h"
+#include "BUS.cpp"
 #include <string>
+#include <math.h>
 
 class ALU {
 public:
-void add_complement(int ind1 , int ind2 , int ind3 , Rigister Rig);
-void add_flowting(int ind1 , int ind2 , int ind3 , Rigister Rig);
-void BitOr(int ind1 , int ind2 , int ind3 , Rigister Rig);
-void BitAnd(int ind1 , int ind2 , int ind3 , Rigister Rig);
-void BitXor(int ind1 , int ind2 , int ind3 , Rigister Rig);
-void Rotate(int ind1 ,int Steps , Rigister Rig);
-bool Greater(int ind1 , Rigister Rig);
-bool EqualTo(int ind1 , Rigister Rig);
+void add_complement(int ind1 , int ind2 , int ind3 , Rigister &Rig);
+void add_flowting(int ind1 , int ind2 , int ind3 , Rigister &Rig);
+void BitOr(int ind1 , int ind2 , int ind3 , Rigister &Rig);
+void BitAnd(int ind1 , int ind2 , int ind3 , Rigister &Rig);
+void BitXor(int ind1 , int ind2 , int ind3 , Rigister &Rig);
+void Rotate(int ind1 ,int Steps , Rigister &Rig);
+bool Greater(int ind1 , Rigister &Rig);
+bool EqualTo(int ind1 , Rigister &Rig);
 string HexTobin(pair<char , char> hexa) ;
 pair<char,char> BintoHex(string num) ;
-float BintoNum(string num) ;
+int BintoNum(string num) ;
 string NumtoBin(float num) ;
+float FBintoNum(string num) ;
+
 
 };
 
