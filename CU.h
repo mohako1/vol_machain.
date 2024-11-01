122efba457b236ee4ec584599d44826e69700185
Msrfr4 <156698572+Msrfr4@users.noreply.github.com>
2024-11-01 22:24:57 +0200

    Add files via upload

---
+++ CU.h
@@ -0,0 +1,22 @@
+#ifndef TASKS_CU_H
+#define TASKS_CU_H
+#include "Memory.h"
+#include "Rigister.h"
+#include "ALU.h"
+#include "PC.h"
+class CU {
+public:
+    CU(){};
+pair<char,char> load_1(int Raddress,int Maddress,Rigister &rig,Memory &mem);
+pair<char,char> load_2(int Raddress,pair<char, char>xy,Rigister &rig);
+pair<char,char> store(int Raddress,int Maddress,Rigister &rig,Memory &mem);
+pair<char,char> move(int Raddress,int raddress,Rigister &rig);
+pair<char,char> operation(const string &op,ALU &al,Rigister &rig,Memory &mem,PC &p);
+void jump(int Raddress,int address,Rigister &rig,PC &p);
+void halt(PC &p);
+
+
+};
+
+
+#endif
