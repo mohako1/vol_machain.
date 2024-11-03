709466c860b84302bf3d1261c4ba64cb614c46d8
Msrfr4 <156698572+Msrfr4@users.noreply.github.com>
2024-11-03 18:31:22 +0200

    Add files via upload

--- Rigister.cpp
+++ Rigister.cpp
@@ -1,21 +1,26 @@
 #include "Rigister.h"
-#include <vector>
-#include <iostream>
-Rigister :: Rigister() :memory(size, {'0', '0'}) {}
 
-     void Rigister::set_value( int key,pair<char,char> value) {
-         memory[key] = value ;
-     }
-    pair<char,char> Rigister :: get_value(int key){
-    return memory[key];
+Rigister::Rigister() : memory(size, {'0', '0'}) {}
+
+pair<char,char> Rigister::set_value(int address,pair<char,char> value1) {
+    pair<char,char> result =  memory [address] = value1;
+    return result;
+    }
+pair<char, char> Rigister :: get_value(int address){
+            return memory[address];
     }
     void Rigister :: display_value() {
         for (int i = 0; i < size; i++) {
-            cout << memory[i].first<< ' ' <<memory[i].second<<'\n';
+            cout << memory[i].first<<memory[i].second<<'\n';
         }
-    }
+}
+
 void Rigister ::reset_memory() {
     for (int i = 0; i < size; i++) {
         memory[i]={'0','0'};
     }
 }
+
+
+
+
