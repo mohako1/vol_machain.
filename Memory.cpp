d559512e9bf59b5764491f6e69f5062fbe2e397f
Msrfr4 <156698572+Msrfr4@users.noreply.github.com>
2024-11-03 18:30:36 +0200

    Add files via upload

--- Memory.cpp
+++ Memory.cpp
@@ -1,12 +1,17 @@
 #include "Memory.h"
 Memory::Memory() : memory(size, {'0', '0'}) {}
 
-void Memory::set_value(int address,pair<char,char> value1) {
-    memory [address] = value1;
+pair<char,char> Memory::set_value(int address,pair<char,char> value1) {
+    pair<char,char>result = memory [address] = value1;
+    return result;
 }
-pair<char, char> Memory :: get_value(int address){
+pair<char, char> Memory::get_value(int address) {
+    if (address < 0 || address >= memory.size()) {
+        throw out_of_range("Address out of bounds");
+    }
     return memory[address];
 }
+
 void Memory :: display_value() {
     for (int i = 0; i < size; i++) {
         cout << memory[i].first<<memory[i].second<<'\n';
