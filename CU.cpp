684c43efb29ca39c5cabec1878c928f03054a49c
Msrfr4 <156698572+Msrfr4@users.noreply.github.com>
2024-11-03 18:28:58 +0200

    Add files via upload

--- CU.cpp
+++ CU.cpp
@@ -1,83 +1,95 @@
 #include "CU.h"
-void CU :: load_1(int Raddress, int Maddress,Rigister &rig,Memory &mem) {
+pair<char,char> CU :: load_1(int Raddress, int Maddress,Rigister &rig,Memory &mem) {
     pair<char,char>value = mem.get_value(Maddress);
-    rig.set_value(Raddress,value);
+    pair<char,char>result = rig.set_value(Raddress,value);
+    return result;
 }
-void CU :: load_2(int Raddress,pair<char, char>xy,Rigister &rig) {
+pair<char,char> CU :: load_2(int Raddress,pair<char, char>xy,Rigister &rig) {
     pair<char,char>value = xy;
-    rig.set_value(Raddress,value);
+    pair<char,char>result = rig.set_value(Raddress,value);
+    return result;
 }
-void CU ::store(int Raddress, int Maddress,Rigister &rig,Memory &mem) {
+pair<char,char> CU ::store(int Raddress, int Maddress,Rigister &rig,Memory &mem) {
     pair<char,char>value = rig.get_value(Raddress);
     if (Maddress == 0) {
-        mem.set_value(0, value);
+        pair<char,char>result =  mem.set_value(0, value);
+        return result;
     }
     else{
-        mem.set_value(Maddress,value);
+        pair<char,char>result = mem.set_value(Maddress,value);
+        return result;
     }
 }
-void CU::move(int Raddress, int raddress,Rigister &rig) {
+pair<char,char> CU::move(int Raddress, int raddress,Rigister &rig) {
     pair<char,char>value = rig.get_value(Raddress);
-    rig.set_value(raddress,value);
-}
-void CU::jump(int Raddress,int address,Rigister &rig,PC &p){
-    if (rig.get_value(Raddress)==rig.get_value(0)){
-        p.set_count(address);
-    }
-    else
-        p.next_count();
+    pair<char,char>result = rig.set_value(raddress,value);
+    return result;
 }
 void CU ::halt(PC &p) {
     p.Set_Zero();
 }
-void CU::operation(const string &op,ALU &al,Rigister &rig,Memory &mem,PC &p) {
+pair<char,char> CU::operation(const string &op,ALU &al,Rigister &rig,Memory &mem,PC &p) {
     pair<char, char> sd = {'0',op[1] };
     string q = al.HexTobin(sd);
-    int rad = al.BintoNum(q);
+    int ind3 = al.BintoNum(q);
 
     pair<char, char> sa = {'0',op[2] };
     string l = al.HexTobin(sa);
-    int rad1 = al.BintoNum(l);
+    int ind1 = al.BintoNum(l);
 
     pair<char, char> sb = {'0',op[3] };
     string f = al.HexTobin(sb);
-    int rad3 = al.BintoNum(f);
+    int ind2 = al.BintoNum(f);
 
     pair<char, char> mad = {op[2], op[3]};
     string m = al.HexTobin(mad);
     int s = al.BintoNum(m);
 
     if (op[0] == '1') {
-         load_1(rad, s, rig, mem);
+         load_1(ind3, s, rig, mem);
     } else if (op[0] == '2') {
-         load_2(rad, {op[2],op[3]}, rig);
+         load_2(ind3, {op[2],op[3]}, rig);
     } else if (op[0] == '3') {
-         store(rad, s, rig, mem);
+         store(ind3, s, rig, mem);
     } else if (op[0] == '4') {
-         move(rad1, rad3, rig);
+         move(ind1, ind2, rig);
     }
     else if (op[0] == '5') {
-        al.add_complement(rad3,rad1, rad, rig);
+        al.add_complement(ind1,ind2, ind3, rig);
     }
     else if (op[0] == '6') {
-        al.add_flowting(rad,rad1, rad3, rig);
+        al.add_flowting(ind1,ind2, ind3, rig);
     }
     else if (op[0] == '7') {
-        al.BitOr(rad,rad1, rad3, rig);
+        al.BitOr(ind1,ind2, ind3, rig);
     }
     else if (op[0] == '8') {
-        al.BitAnd(rad,rad1, rad3, rig);
+        al.BitAnd(ind1,ind2, ind3, rig);
     }
     else if (op[0] == '9') {
-        al.BitXor(rad,rad1, rad3, rig);
+        al.BitXor(ind1,ind2, ind3, rig);
     }
     else if (op[0] == 'A') {
-       al.Rotate(rad1,rad3,rig);
+       al.Rotate(ind3,ind2,rig);
     }
     else if (op[0]=='B'){
-        jump(rad,s,rig,p);
+        if (al.EqualTo(ind3,rig)){
+            p.set_count(s);
+        }
+        else{
+            p.next_count();
+        }
+    }
+    else if (op[0]=='D'){
+        if (al.Greater(ind3,rig)){
+           p.set_count(s);
+        }
+        else{
+            p.next_count();
+        }
     }
     else if (op[0]=='C'){
         halt(p);
     }
+    return {'0','0'};
 }
